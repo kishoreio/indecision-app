22b3315b9ac7204bf4ded7b6b7e145ea2bb733b9
Kishore S <kishore@Kishores-MacBook-Pro.local>
2020-02-23 22:38:44 +0530

    Updates

---
+++ precache-manifest.cb489611cb254bada5fb924aaac20c63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e31d70070652ea7a2a486f3eadb9b4c",
+    "url": "/indecision-app/index.html"
+  },
+  {
+    "revision": "b928cb6acd17ef0d75ef",
+    "url": "/indecision-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8d50829465d476c7f7ce",
+    "url": "/indecision-app/static/css/main.778792b9.chunk.css"
+  },
+  {
+    "revision": "b928cb6acd17ef0d75ef",
+    "url": "/indecision-app/static/js/2.ecfd70bc.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/indecision-app/static/js/2.ecfd70bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d50829465d476c7f7ce",
+    "url": "/indecision-app/static/js/main.ee5fa277.chunk.js"
+  },
+  {
+    "revision": "7863dc0744ebc4ad44fc",
+    "url": "/indecision-app/static/js/runtime-main.8d787059.js"
+  }
+]);
